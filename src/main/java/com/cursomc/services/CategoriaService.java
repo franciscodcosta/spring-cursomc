b50bf06cc26d8c4d2be2d55269f3e7f3c088c1bb
Francisco Daniel Costa <franciscodcosta@yahoo.com.br>
2019-03-11 18:13:51 -0300

    atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/cursomc/services/CategoriaService.java
+++ src/main/java/com/cursomc/services/CategoriaService.java
@@ -23,8 +23,12 @@ public class CategoriaService {
 
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
-		repo.save(obj);
-		return obj;
+		return repo.save(obj);
+	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
 	}
 
 }

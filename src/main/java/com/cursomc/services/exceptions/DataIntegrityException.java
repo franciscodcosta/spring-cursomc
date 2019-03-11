e50a1917ff925b98498b83574b4279a5627a45d2
Francisco Daniel Costa <franciscodcosta@yahoo.com.br>
2019-03-11 18:33:29 -0300

    deletando uma Categoria com DELETE

---
+++ src/main/java/com/cursomc/services/exceptions/DataIntegrityException.java
@@ -0,0 +1,19 @@
+package com.cursomc.services.exceptions;
+
+public class DataIntegrityException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DataIntegrityException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataIntegrityException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+}

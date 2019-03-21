eb41725e947b8685ef0e8a41bb574823dd355162
Francisco Daniel Costa <franciscodcosta@yahoo.com.br>
2019-03-21 09:10:57 -0300

    validação de dados com BeanValidation

--- src/main/java/com/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
@@ -9,6 +13,9 @@ public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message="o nome não pode estar em branco")
+	@Length(min=5, max=80, message="o tamanho do nome deve estar entre 5 e 80")
 	private String nome;
 	
 	public CategoriaDTO() {

3b2d46a920a205a51730725b6197ace887a330cb
Francisco Daniel Costa <franciscodcosta@yahoo.com.br>
2019-04-01 22:31:11 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/cursomc/services/ClienteService.java
+++ src/main/java/com/cursomc/services/ClienteService.java
@@ -57,7 +57,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir um Cliente que tem produtos");
+			throw new DataIntegrityException("Não é possível excluir um Cliente que tem pedidos");
 		}
 
 	}
